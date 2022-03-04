e416415dd230a00b44e5bc4a9624d4ea438d96a8
jbmitchell7 <jbmitchell747@gmail.com>
2022-03-04 12:15:10 -0600

    added redirect for login and a logout function

--- src/components/Account/Account.js
+++ src/components/Account/Account.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { View, StyleSheet, Linking } from 'react-native';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { Text, Button } from 'react-native-paper';
@@ -7,6 +7,7 @@ import { fetchPost } from '../../api/tmdb';
 import colors from '../../assets/colors';
 
 const Account = () => {
+  const [loginStatus, setLoginStatus] = useState(false);
 
   const createRequest = async () => {
     try {
@@ -15,6 +16,7 @@ const Account = () => {
       await AsyncStorage.setItem('token', token.request_token);
     }
     catch (error) {
+      //TODO add snack bar 
       console.log('error getting request token');
       console.log(error);
     }
@@ -23,41 +25,70 @@ const Account = () => {
   const loginToken = async (token) => {
     try {
       const asyncToken = await AsyncStorage.getItem('token');
-      const token = await fetchPost(`/4/auth/access_token`, { request_token: asyncToken });
-      //TODO set to redirect to user account page
-      console.log(`logged in`);
+      await fetchPost(`/4/auth/access_token`, { request_token: asyncToken });
+      setLoginStatus(true);
+      //TODO add snack bar 
     }
     catch {
-      console.log('error saving token to asyncstorage');
+      //TODO add snack bar 
+      console.log('error logging in');
     }
+  }
+
+  const logout = async () => {
+    try {
+      await AsyncStorage.setItem('token', '');
+      setLoginStatus(false);
+      //TODO add snack bar 
+    }
+    catch {
+      //TODO add snack bar 
+      console.log('failed to log out');
+    }
+  }
+
+  if (!loginStatus) {
+    return (
+      <View>
+        <Text style={styles.header}>Login</Text>
+        <Text style={styles.text}>
+          Step 1: Create an account at tmdb.org
+        </Text>
+        <Text style={styles.text}>
+          Step 2: Approve access to your TMDB account
+        </Text>
+        <Text style={styles.text}>
+          Step 3: Login
+        </Text>
+        <Button
+          color={colors.yellow}
+          mode='contained'
+          style={styles.btn}
+          onPress={() => createRequest()}>
+          Approve Access
+        </Button>
+        <Button
+          color={colors.yellow}
+          // disabled={true}
+          mode='contained'
+          style={styles.btn}
+          onPress={() => loginToken()}>
+          Login
+        </Button>
+      </View>
+    )
   }
 
   return (
     <View>
       <Text style={styles.header}>Account</Text>
-      <Text style={styles.text}>
-        Step 1: Create an account at tmdb.org
-      </Text>
-      <Text style={styles.text}>
-        Step 2: Approve access to your TMDB account
-      </Text>
-      <Text style={styles.text}>
-        Step 3: Login
-      </Text>
-      <Button
-        color={colors.yellow}
-        mode='contained'
-        style={styles.btn}
-        onPress={() => createRequest()}>
-        Approve Access
-      </Button>
       <Button
         color={colors.yellow}
         // disabled={true}
         mode='contained'
         style={styles.btn}
-        onPress={() => loginToken()}>
-        Login
+        onPress={() => logout()}>
+        Logout
       </Button>
     </View>
   )
